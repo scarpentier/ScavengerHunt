1aa99d80f61df8adf1def4e293c43e1da4983409
Bach Nguyen-Ngoc <bnguyen@lanets.ca>
2017-01-15 16:58:54 -0500

    added upstream

--- ScavengerHunt.Web/Controllers/TeamStuntController.cs
+++ ScavengerHunt.Web/Controllers/TeamStuntController.cs
@@ -118,11 +118,6 @@ namespace ScavengerHunt.Web.Controllers
 
         public ActionResult Summary()
         {
-            if (!Settings.GuestSummaryVisible && !User.IsInRole("Admin"))
-                            {
-                ModelState.AddModelError("Name", "Team Stunts Summary is disabled");
-                                return View("Start");
-                            }
             // Filter and sort stunts
             var stunts =
                 db.TeamStunts.Where(x => x.Stunt.Published)
